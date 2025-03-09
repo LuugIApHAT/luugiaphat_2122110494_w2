2a7b8a03a7a0a4f2e2fdad4e9458afb52e8b13b1
LuugIApHAT <luugiaphat2083@gmail.com>
2025-03-09 19:24:36 +0700

    first commit

---
+++ src/main/java/com/example/LuuGiaPhat/LuuGiaPhatApplication.java
@@ -0,0 +1,14 @@
+package com.example.LuuGiaPhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuuGiaPhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuuGiaPhatApplication.class, args);
+		System.out.println("HeloHelo");
+	}
+
+}
---
+++ src/test/java/com/example/LuuGiaPhat/LuuGiaPhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LuuGiaPhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuuGiaPhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
